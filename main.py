01e4dd71016327ebc3bbb70addc11f28c36667dd
7ima_SR <ibrahimelsaeed177@gmail.com>
2026-08-09 18:19:53 +0300

    Stage 1: signup and login routes working

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from supabase import create_client, Client
+from pydantic import BaseModel
 
 load_dotenv()
 
@@ -19,9 +20,63 @@ supabase:Client = create_client(
 
 app = FastAPI()
 
+class AuthRequest(BaseModel):
+    email: str
+    password: str
+
 @app.get("/")
 def root():
     return{
         "name": "Auth API",
         "Status": "Server running and connected to Supabase successfully",
     }
+
+@app.post("/auth/signup", status_code=201)
+def signup(data: AuthRequest):
+    if not data.email.strip() or not data.password.strip():
+        raise HTTPException(
+            status_code=400,
+            detail="Email and password are required"
+        )
+
+    try:
+        response = supabase.auth.sign_up({
+            "email": data.email,
+            "password": data.password
+        })
+
+        return {
+            "user": response.user
+        }
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=400,
+            detail=str(e)
+        )
+
+
+@app.post("/auth/login")
+def login(data: AuthRequest):
+    if not data.email.strip() or not data.password.strip():
+        raise HTTPException(
+            status_code=400,
+            detail="Email and password are required"
+        )
+
+    try:
+        response = supabase.auth.sign_in_with_password({
+            "email": data.email,
+            "password": data.password
+        })
+
+        return {
+            "access_token": response.session.access_token,
+            "refresh_token": response.session.refresh_token
+        }
+
+    except Exception:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid login credentials"
+        )
